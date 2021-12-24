2ee5397b1d11c50d8fa53cde5e215b7fcf4d52b8
ppforde <ppforde@outlook.com>
2021-12-24 10:07:18 -0500

    Updated Perl example

--- autoloans/autoloan_perl/main.pl
+++ autoloans/autoloan_perl/main.pl
@@ -2,8 +2,7 @@
 
 
 my $rate = 4.7;
-my $pv = 25000;
-my @nper = (12, 24, 36, 48, 60, 72);
+my @nper = (24, 36, 48, 60, 72);
 
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
 
@@ -14,6 +13,12 @@ my $file = 'loans-perl.csv';
 open $fh, '>', $file
     or die "Fille Error : $1";
 print $fh "PV,NPER,RATE,PMT,STATUS,DATE\n";
+
+my $pv = 25000;
+
+my $countall = 0;
+my $sumall = 0.0;
+
 
 while ($pv < 30000) {
     for my $x (@nper) {
@@ -32,9 +37,17 @@ while ($pv < 30000) {
        printf("pv => %d, nper => %d, rate => %0.2f = pmt of %0.2f is %s on %s\n", $pv, $x, $rate, $pmt, $status, $datestr);
        $line = sprintf("%d,%d,%0.2f,%0.2f,%s,%s\n", $pv, $x, $rate, $pmt, $status, $datestr);
        print $fh $line;
-    }
 
+        $countall = $countall + 1;
+        $sumall = $sumall + $pmt;    
+
+    } 
+    
     $pv = $pv + 1000;
 }
 
 close $fh or die "Couldn't Close File : $!";
+
+printf("Total records %d and total payments \$%0.2f\n", $countall, $sumall)
+
+# perl main.pl
